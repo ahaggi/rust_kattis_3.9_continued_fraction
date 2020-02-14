e58d11450f48cbb0c2e3b963204250c56b897d25
ahaggi <a.haggi@yahoo.com>
2020-02-14 18:37:51 +0100

    first commit

---
+++ readme.md
@@ -0,0 +1,59 @@
+
+# Continued Fraction
+
+The (simple) continued fraction representation of a real number r is an expression obtained by an iterative process of representing r as the sum of its integer part and the reciprocal of another number, then writing this other number as the sum of its integer part and another reciprocal, and so on. In other words, a continued fraction representation of r is of the form 
+
+![alt text][img1]
+
+where a<sub>0</sub>, a<sub>1</sub>, a<sub>2</sub>, ...  are integers and a<sub>1</sub>, a<sub>2</sub>, ... > 0. We call the a_ i-values _partial quotients_. For example, in the continued fraction representation of 5.4, the partial quotients are a<sub>0</sub> = 5, a<sub>1</sub> = 2, a<sub>2</sub> = 2. This representation of a real number has several applications in theory and practice. If r is a rational number, the partial quotients are eventually all zero, so we only need to consider a finite number of partial quotients.
+
+Given two rational numbers in continued fraction representation, your task is to perform the four elementary arithmetic operations on these numbers and display the results in continued fraction representation.
+
+## Input
+
+The input consists of a single test case. The test case consists of three lines. The first line contains two integers n<sub>1</sub> and n<sub>2</sub>, where 1  <= n_ i  <= 9 is the number of partial quotients of rational number r_ i for 1  <= i  <= 2. The second line contains the partial quotients of r<sub>1</sub> and the third line contains the partial quotients of r<sub>2</sub>. The absolute values of the quotients are not more than 10 and you may assume that r<sub>1</sub> > r<sub>2</sub> > 0.
+
+## Output
+
+Display the partial quotients of the continued fraction representations of r<sub>1</sub>+r<sub>2</sub>, r<sub>1</sub>-r<sub>2</sub>, r<sub>1</sub> X r<sub>2</sub>, and r<sub>1</sub> / r<sub>2</sub>, in order, each in a line. Consecutive partial quotients on each line are separated by a single space. Do not print any trailing zero partial quotients.
+
+<table  summary="sample data">
+
+<tbody>
+
+<tr>
+
+<th>Sample Input 1</th>
+
+<th>Sample Output 1</th>
+
+</tr>
+
+<tr>
+
+<td>
+
+<pre>4 3
+5 1 1 2
+5 2 2
+</pre>
+
+</td>
+
+<td>
+
+<pre>11
+0 5
+30 4 6
+1 27
+</pre>
+
+</td>
+
+</tr>
+
+</tbody>
+
+</table>
+
+[img1]: .\img1.png
---
+++ src/main.rs
@@ -0,0 +1,141 @@
+use std::io;
+
+fn main() {
+    let stdin = io::stdin();
+    
+    // ignore the fst line
+    stdin
+        .read_line(&mut " ".to_string())
+        .expect("Failed to read line");
+    
+     let fst: Vec<i64> = read_line_vec_i64();
+     let snd: Vec<i64> = read_line_vec_i64();
+    
+    let fst: (i64, i64) = gen_simple_fraction_top_down(&fst);
+    let snd: (i64, i64) = gen_simple_fraction_top_down(&snd);
+    println!("{:?}", fst);
+    println!("{:?}", snd);
+    
+    let add = add_fractions(fst, snd);
+    let sub = sub_fractions(fst, snd);
+    let mult = mult_fractions(fst, snd);
+    let div = div_fractions(fst, snd);
+    
+    println!("add {:?}", add);
+    println!("sub {:?}", sub);
+    println!("mult {:?}", mult);
+    println!("div {:?}", div);
+    
+    for term in gen_continued_fraction(add) {
+        print!(" {}", term);
+    }
+    println!("");
+    for term in gen_continued_fraction(sub) {
+        print!(" {}", term);
+    }
+    println!("");
+    for term in gen_continued_fraction(mult) {
+        print!(" {}", term);
+    }
+    println!("");
+    for term in gen_continued_fraction(div) {
+        print!(" {}", term);
+    }
+    println!("");
+}
+
+fn gen_simple_fraction_top_down(data: &[i64]) -> (i64, i64) {
+    // For a given term a(k), we can compute a result p(k) / q(k)
+    // We always start by defining p and q for k=0 and k=-1
+    // p(-1) = 0, q(-1) = 1
+    // p( 0) = 1, q( 0) = 0
+    //
+    // p(k) = a * p(-1) + p(-2)
+    // q(k) = a * q(-1) + q(-2)
+    
+    let mut res = [(0, 1), (1, 0)];
+    
+    for term in data.iter() {
+        // println!("{:#?}/{:#?}", res[1].0, res[1].1);
+        let p = term * res[1].0 + res[0].0;
+        let q = term * res[1].1 + res[0].1;
+        res[0] = res[1];
+        res[1] = (p, q);
+    }
+    // println!("{:#?}/{:#?}", res[1].0, res[1].1);
+    res[1]
+}
+
+fn gen_continued_fraction((p, q): (i64, i64)) -> Vec<i64> {
+    let negate = (p > 0 && q < 0) || (p < 0 && q > 0);
+    let mut v: Vec<i64> = vec![];
+    let mut p = p.abs();
+    let mut q = q.abs();
+    let mut temp = p % q;
+    
+    if temp == 0 {
+        v.push(p / q);
+    } else {
+        while temp != 0 {
+                v.push(p / q);
+                temp = p % q;
+                p = q;
+                q = temp;
+            }
+        }
+        if negate {
+            if v.len() == 1 {
+                v[0] *= -1;
+            } else {
+                v[0] = -v[0] - 1;
+                
+                if v[1] == 1 {
+                    v.remove(1);
+                    v[1] += 1;
+                } else {
+                    v.insert(1, 1);
+                    v[2] -= 1;
+                }
+            }
+        }
+        // println!("{:#?}", v);
+        v
+    }
+    
+    fn add_fractions((p1, q1): (i64, i64), (p2, q2): (i64, i64)) -> (i64, i64) {
+        let p1 = p1.checked_mul(q2).expect("overflow!");
+        let p2 = p2.checked_mul(q1).expect("overflow!");
+        let q = q1 .checked_mul(q2).expect("overflow!");
+        (p1 + p2, q)
+    }
+    
+    fn sub_fractions((p1, q1): (i64, i64), (p2, q2): (i64, i64)) -> (i64, i64) {
+        let p1 = p1.checked_mul(q2).expect("overflow!");
+        let p2 = p2.checked_mul(q1).expect("overflow!");
+        let q = q1 .checked_mul(q2).expect("overflow!");
+        (p1 - p2, q)
+    }
+    
+    fn mult_fractions((p1, q1): (i64, i64), (p2, q2): (i64, i64)) -> (i64, i64) {
+        (p1.checked_mul(p2).expect("overflow!") , q1.checked_mul(q2).expect("overflow!"))
+    }
+    
+    fn div_fractions((p1, q1): (i64, i64), (p2, q2): (i64, i64)) -> (i64, i64) {
+        (p1.checked_mul(q2).expect("overflow!") , q1.checked_mul(p2).expect("overflow!"))
+    }
+    
+    fn read_line_vec_i64() -> Vec<i64> {
+        let stdin = io::stdin();
+        
+        let mut val = String::new();
+        stdin.read_line(&mut val).expect("Failed to read line");
+        
+        let line: Vec<i64> = val
+        .trim()
+        .split_whitespace()
+        .map(|num| num.parse::<i64>().unwrap())
+        .collect();
+        
+        line
+    }
+    
